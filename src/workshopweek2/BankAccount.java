5f68e3136cfea5a26c2a3fe4527c26d8d87d27f6
Cesar David Millan <cemillan@bancolombia.com.co>
2024-05-20 17:39:55 -0500

    Working on workshop 2

--- src/workshopweek2/BankAccount.java
+++ src/workshopweek2/BankAccount.java
@@ -1,6 +1,9 @@
 package workshopweek2;
 
+import java.awt.*;
+import java.time.LocalDate;
 import java.util.*;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -14,7 +17,86 @@ public class BankAccount {
     public void addTransaction(Transaction transaction) {
         transactions.add(transaction);
     }
-    // TODO 9: Implementar getTransactionsWithAmountGreaterThan utilizando streams y filter
+
+    // 1. Implementar getTotalBalance utilizando streams y reduce
+    public Optional<Double> getTotalBalance() {
+
+        Map<String, Function<Transaction, Double>> transactionHandler = new HashMap<>();
+        transactionHandler.put("deposit", Transaction::getAmount);
+        transactionHandler.put("withdrawal", transaction -> -transaction.getAmount());
+
+        return Optional.of(transactions.stream()
+                .mapToDouble(transaction ->
+                        Optional.ofNullable(transactionHandler.get(transaction.getType()))
+                                .map(handler -> handler.apply(transaction))
+                                .orElse(0.0))
+                .reduce(0.0, Double::sum));
+    }
+
+    //2. Implementar getDeposits utilizando streams y filter
+    public Optional<List<Transaction>> getDeposits() {
+        return Optional.of(transactions
+                .stream()
+                .filter(transactions -> transactions.getType().equals("deposit")))
+                .map(transactions -> transactions.toList());
+    }
+
+    //3. Implementar getWithdrawals utilizando streams y filter
+    public Optional<List<Transaction>> getWithdrawals() {
+        return Optional.of(transactions
+                .stream()
+                .filter(transactions ->transactions.getType().equals("withdrawal")))
+                .map(transactions -> transactions.toList());
+    }
+
+    // TODO 4: Implementar filterTransactions utilizando Function y streams
+    public Optional<List<Transaction>> filterTransactions(Function<Transaction, Boolean> predicate) {
+        // TODO 4
+        return Optional.of(transactions
+                .stream()
+                .filter(predicate::apply)
+                .collect(Collectors.toList()));
+    }
+
+    // 5. Implementar getTotalDeposits utilizando getDeposits y mapToDouble
+    public Optional<Double> getTotalDeposits() {
+        return getDeposits().map(deposits ->
+                deposits.stream()
+                        .mapToDouble(Transaction::getAmount)
+                        .sum());
+    }
+
+    // 6. Implementar getLargestWithdrawal utilizando getWithdrawals y max
+    public Optional<Transaction> getLargestWithdrawal() {
+        return getWithdrawals()
+                .flatMap(withdrawals ->
+                        withdrawals.stream()
+                                .max(Comparator.comparing(Transaction::getAmount)));
+
+    }
+
+    //7. Implementar getTransactionsOnDate utilizando streams y filter
+    public Optional<List<Transaction>> getTransactionsOnDate(LocalDate date) {
+        return Optional.of(transactions
+                .stream()
+                .filter(transaction -> transaction.getDate().equals(date))
+                .toList());
+    }
+
+    // 8. Implementar getAverageTransactionAmount utilizando streams y mapToDouble
+    public OptionalDouble getAverageTransactionAmount() {
+        Map<String, Function<Transaction, Double>> transactionHandler = new HashMap<>();
+        transactionHandler.put("deposit", Transaction::getAmount);
+        transactionHandler.put("withdrawal", transaction -> -transaction.getAmount());
+
+        return OptionalDouble.of(transactions
+                .stream()
+                .mapToDouble(transactions -> transactions.getAmount())
+                .average()
+                .getAsDouble());
+    }
+
+    //9. Implementar getTransactionsWithAmountGreaterThan utilizando streams y filter
     public Optional<List<Transaction>> getTransactionsWithAmountGreaterThan(double amount) {
         return Optional.of(transactions.stream()
                 .filter(transaction -> transaction.getAmount() > amount)
@@ -24,13 +106,19 @@ public class BankAccount {
     // TODO 10: Implementar transfer utilizando addTransaction
     public static void transfer(BankAccount sourceAccount, BankAccount targetAccount, double amount) {
         // TODO 10
+    /**
+        sourceAccount.getTotalBalance()
+                .stream()
+                .mapToDouble()
+
+
+**/
+
 
     }
 
-    // TODO 11: Implementar getTotalWithdrawals utilizando getWithdrawals y mapToDouble
+    // 11. Implementar getTotalWithdrawals utilizando getWithdrawals y mapToDouble
     public Optional<Double> getTotalWithdrawals() {
-        // TODO 11
-
         Map<String, Function<Transaction, Double>> transactionHandler = new HashMap<>();
         transactionHandler.put("withdrawal", transaction -> -transaction.getAmount());
 
@@ -44,7 +132,7 @@ public class BankAccount {
     // TODO 12: Implementar getTransactionsSummary utilizando streams, map y collect
     public Map<String, Double> getTransactionsSummary() {
         // TODO 12
-
+        return null;
     }
 
 }
--- src/workshopweek2/Main.java
+++ src/workshopweek2/Main.java
@@ -1,4 +1,99 @@
 package workshopweek2;
 
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        var bank = new BankAccount();
+
+        Transaction t1 = new Transaction();
+        t1.setAmount(1000.0);
+        t1.setType("deposit");
+        t1.setDate(LocalDate.of(2024, 5, 1));
+        bank.addTransaction(t1);
+
+        Transaction t2 = new Transaction();
+        t2.setAmount(200.0);
+        t2.setType("withdrawal");
+        t2.setDate(LocalDate.of(2024, 5, 2));
+        bank.addTransaction(t2);
+
+        Transaction t3 = new Transaction();
+        t3.setAmount(500.0);
+        t3.setType("deposit");
+        t3.setDate(LocalDate.of(2024, 5, 3));
+        bank.addTransaction(t3);
+
+        Transaction t4 = new Transaction();
+        t4.setAmount(300.0);
+        t4.setType("withdrawal");
+        t4.setDate(LocalDate.of(2024, 5, 4));
+        bank.addTransaction(t4);
+
+
+        Transaction t5 = new Transaction();
+        t5.setAmount(450.0);
+        t5.setType("deposit");
+        t5.setDate(LocalDate.of(2024, 7, 15));
+        bank.addTransaction(t5);
+
+        Transaction t6 = new Transaction();
+        t6.setAmount(250.0);
+        t6.setType("withdrawal");
+        t6.setDate(LocalDate.of(2024, 3, 3));
+        bank.addTransaction(t6);
+
+
+        var bank2 = new BankAccount();
+
+        Transaction t7 = new Transaction();
+        t7.setAmount(1000.0);
+        t7.setType("deposit");
+        t7.setDate(LocalDate.of(2024, 5, 1));
+        bank2.addTransaction(t7);
+
+
+        //Verifications
+
+        //1 Total balance
+        System.out.println("Total balance ->"+bank.getTotalBalance());
+
+
+        //2
+        System.out.println("---- Deposits " + bank.getDeposits());
+
+        //3 GetWinthdrawals
+
+        System.out.println("---- Withdrawals " + bank.getWithdrawals());
+
+
+        // 4 filter by type
+
+
+
+        //5
+        System.out.println("total deposits -> " + bank.getTotalDeposits());
+
+        //&
+        System.out.println("Largest withdrawal -> " + bank.getLargestWithdrawal());
+
+        //7
+        System.out.println("date "+bank.getTransactionsOnDate(LocalDate.of(2024, 5, 4)));
+
+        //8 getaverage()
+        System.out.println("average -> " + bank.getAverageTransactionAmount());
+
+
+        //9
+        System.out.println("greater amount "+ bank.getTransactionsWithAmountGreaterThan(100));
+
+
+        //11 total get withdrawals
+
+        System.out.println("total withdrawals -> " + bank.getTotalWithdrawals());
+
+
+    }
 }
